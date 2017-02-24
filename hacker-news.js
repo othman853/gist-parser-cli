1ca42b64431288b19dd5a26acb2711691930fc92
othman853 <anuar.yasser@gmail.com>
2017-02-24 20:31:10 -0300

    Decoding parsed html

--- hacker-news.js
+++ hacker-news.js
@@ -1,17 +1,15 @@
 const got = require('got');
 const cheerio = require('cheerio');
 const chalk = require('chalk');
-
+const entities = require('entities');
 
 got('https://news.ycombinator.com').then(response => {
 
   const $ = cheerio.load(response.body);
 
-  const news = $('.itemlist > .athing');
-
-  news
+  $('.itemlist > .athing')
     .map( (_, a) => $(a) )
-    .map( (_, a) => ({ rank: a.find('.rank').first().html(), txt: a.find('.storylink').first().html() }) )
+    .map( (_, a) => ({ rank: a.find('.rank').first().html(), txt: entities.decodeHTML(a.find('.storylink').first().html()) }) )
     .map( (_, a) => `${a.rank}${a.txt}` )
     .map( (_, a) => chalk.green(a) )
     .each( (_, a) => console.log(a));
