3d7e13bc702a7f44747eb7ecea5ea12ea7889f38
othman853 <anuar.yasser@gmail.com>
2017-02-28 00:28:36 -0300

    Weather in portuguese

--- weather.js
+++ weather.js
@@ -2,5 +2,5 @@ const got = require('got');
 
 const headers = { 'User-Agent': 'curl/7.43.0' };
 
-got('wttr.in', { headers })
+got('wttr.in?lang=pt', { headers })
   .then(response => console.log(response.body));
